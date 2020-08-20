f53c7eb049ae5e861b642625ecbb676b7f36b996
Harouna Diallo <dev.harouna@gmail.com>
2020-08-20 02:27:07 +0000

    add method call

--- android/src/main/java/com/jaalee/sdk/RNAndroidJaaleeModule.java
+++ android/src/main/java/com/jaalee/sdk/RNAndroidJaaleeModule.java
@@ -6,9 +6,19 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
-public class RNAndroidJaaleeModule extends ReactContextBaseJavaModule {
+import android.content.Context;
+
+import com.jaalee.sdk.connection.BeaconConnection;
+import com.jaalee.sdk.service.BeaconService;
+import com.jaalee.sdk.connection.ConnectionCallback;
+import com.jaalee.sdk.Beacon;
+import com.jaalee.sdk.BeaconManager;
+
+public class RNAndroidJaaleeModule extends ReactContextBaseJavaModule implements ConnectionCallback {
 
   private final ReactApplicationContext reactContext;
+  private static Beacon beacon = null;
+  private static BeaconConnection statusBeacon = null;
 
   public RNAndroidJaaleeModule(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -19,4 +29,19 @@ public class RNAndroidJaaleeModule extends ReactContextBaseJavaModule {
   public String getName() {
     return "RNAndroidJaalee";
   }
+
+  @Override
+  public void StartBeaconConnectionWithPassword(String proximityUUID, String name, String macAddress,
+      int major, int minor, int measuredPower, int rssi, int batt, String password){
+    this.beacon = Beacon.Beacon(proximityUUID, name, macAddress, major, minor, measuredPower, rssi, batt);
+    this.statusBeacon = BeaconConnection(this.reactContext, this.beacon, ConnectionCallback);
+    this.statusBeacon.connectBeaconWithPassword(password);
+  }
+
+  @Override
+  public void CallBeaconImata(){
+    this.statusBeacon.CallBeacon();
+  }
+
+
 }
